5098dd0a3aa31459c283a8ee32238836e2ebf287
bakari <anbphones@icloud.com>
2025-11-23 11:49:13 +0300

    second dubbingpractice

---
+++ debuggingarea.js
@@ -0,0 +1,15 @@
+// area.js
+
+function calculateArea(length, width) {
+
+  debugger; // Pause execution to inspect variables
+
+  let area = length * width;
+
+  return area;
+
+}
+
+let area = calculateArea(5, 10);
+
+console.log('Area of the rectangle:', area); // Should output 50node de
